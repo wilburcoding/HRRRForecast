414cccaf7f692236d9fa2b8f2d0f3ae11c436e40
Wilbur W <wilburwei.2008@gmail.com>
2024-08-04 18:01:15 -0400

    Edit gitignore + finish gif creator

--- to_gif.py
+++ to_gif.py
@@ -2,7 +2,9 @@ import os
 import imageio
 path = "./output"
 dir_list = os.listdir(path)
+print("Gif Creator")
+ms = int(input("Delay (ms):"))
 images = []
 for filename in dir_list:
     images.append(imageio.imread("./output/" + filename))
-imageio.mimsave('output.gif', images, duration=0.8)
+imageio.mimsave('output.gif', images, duration=float(ms/1000))
